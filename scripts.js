3857828d0b7113c96fe45fc922d49f8cb92e4108
Evan Norton <evanan39@yahoo.com>
2018-01-17 18:34:26 -0600

    setText() method to decide which character displays based on value

--- scripts.js
+++ scripts.js
@@ -3,14 +3,31 @@ let dice = [];
 class Die {
     constructor() {
         this.roll();
+        this.setText();
         dice.push(this);
-        this.div = $("<div>&#9856</div>");
+        this.div = $("<div>" + this.text + "</div>");
         $(this.div).addClass("die");
         $(this.div).appendTo("#dice-container");
     }
 
     roll() {
         this.value = Math.floor(Math.random() * 6 + 1);
+    }
+
+    setText() {
+        if (this.value === 1 ) {
+            this.text = "&#9856";
+        } else if (this.value === 2) {
+            this.text = "&#9857"
+        } else if (this.value === 3) {
+            this.text = "&#9858";
+        } else if (this.value === 4 ) {
+            this.text = "&#9859";
+        } else if (this.value === 5) {
+            this.text = "&#9860";
+        } else {
+            this.text = "&#9861";
+        }
     }
 }
 
