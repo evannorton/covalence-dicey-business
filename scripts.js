4431ff6221da8bd6c6fabaef3aacdd7448471c73
Evan Norton <evanan39@yahoo.com>
2018-01-17 19:29:16 -0600

    sum dice button adds up values of all dice on screen and alerts

--- scripts.js
+++ scripts.js
@@ -1,44 +1,54 @@
 let dice = [];
 let btnGenerate = $("#btn-generate");
 let btnRoll = $("#btn-roll");
+let btnSum = $("#btn-sum");
 
 class Die {
     constructor() {
-        //create div
         this.div = $("<div></div>")
-        //call roll method
         this.roll();
-        //add div to screen
         $(this.div).appendTo("#dice-container");
         dice.push(this);
     }
 
     roll() {
         this.value = Math.floor(Math.random() * 6 + 1);
-        if (this.value === 1 ) {
-            this.value = "⚀";
-        } else if (this.value === 2) {
-            this.value = "⚁"
-        } else if (this.value === 3) {
-            this.value = "⚂";
-        } else if (this.value === 4 ) {
-            this.value = "⚃";
-        } else if (this.value === 5) {
-            this.value = "⚄";
-        } else {
-            this.value = "⚅";
-        }
-        $(this.div).text("" + this.value + "");
+        this.getChar();
+        $(this.div).text("" + this.char + "");
         $(this.div).addClass("die");
+    }
+
+    getChar() {
+        if (this.value === 1) {
+            this.char = "⚀";
+        } else if (this.value === 2) {
+            this.char = "⚁"
+        } else if (this.value === 3) {
+            this.char = "⚂";
+        } else if (this.value === 4) {
+            this.char = "⚃";
+        } else if (this.value === 5) {
+            this.char = "⚄";
+        } else {
+            this.char = "⚅";
+        }
     }
 }
 
-$(btnGenerate).click(function() {
+$(btnGenerate).click(() => {
     let die = new Die();
 });
 
-$(btnRoll).click(function() {
+$(btnRoll).click(() => {
     for (let i = 0; i < dice.length; i++) {
         dice[i].roll();
     }
+});
+
+$(btnSum).click(() => {
+    let sum = 0;
+    for (let i = 0; i < dice.length; i++) {
+        sum += dice[i].value;
+    }
+    alert(`The sum of the values of all dice on the screen is ${sum}.`);
 });
