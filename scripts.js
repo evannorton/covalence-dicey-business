4b13c44dbd61000c8c9cfd838d7991876ac75331
Evan Norton <evanan39@yahoo.com>
2018-01-17 19:34:53 -0600

    individual die rerolls on click

--- scripts.js
+++ scripts.js
@@ -7,6 +7,9 @@ class Die {
     constructor() {
         this.div = $("<div></div>")
         this.roll();
+        $(this.div).click(() => {
+            this.roll();
+        });
         $(this.div).appendTo("#dice-container");
         dice.push(this);
     }
